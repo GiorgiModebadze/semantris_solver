0eaff65c880e33b385449ae45c4fe399c1862c2d
Giorgi Modebadze <giorgi.modebadze@hu-berlin.de>
2020-05-28 21:37:35 +0200

    end of the day

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import requests
+import json
+from selenium.webdriver.common.keys import Keys
 
 
 driver = webdriver.Chrome("./chromedriver")
@@ -9,17 +12,49 @@ driver.get("https://research.google.com/semantris/")
 #TODO : Find the way how to press the button automatically 
 time.sleep(15)
 
-for i in range (10):
+wordlist = []
+words = []
+index = 0
 
-    TARGET =  driver.execute_script("return this.game.currentGame.targetLines")
-    print(TARGET[0])
+target_index = 0
+
+while True:
+
+    TARGET = []
+    while len(TARGET) == 0:  
+        TARGET =  driver.execute_script("return this.game.currentGame.targetLines")
+
+    if target_index > 0 and len(TARGET) >= target_index and index>=len(words):
+        target_index = 0
+
+    print("target", TARGET)
+    TARGET = TARGET[target_index].replace(' ', '+')
     
-    #TODO : Find the best way for finding best matching word. 
+    wordlist.append(TARGET)
 
-    bestMatch = "person"
+
+    if len(wordlist) > 1 and wordlist[-1] == wordlist[-2]:
+        index += 1
+    else:
+        words = requests.get(f'http://api.datamuse.com/words?ml={TARGET}').json()
+        index = 0
+
+    # out of bound so we move to next word
+    if len(words) <= index:
+        target_index+=1
+        continue
+    
+
+    bestMatch = words[index]['word']
+    print("Best Match", bestMatch)
+
+    if bestMatch[:3].lower() in TARGET.lower():
+        print("We Continues")
+        index +=1
+        continue
+
     driver.execute_script(f'this.game.currentGame.userSubmit("{bestMatch}","{bestMatch}")')
+    time.sleep(2 - len(wordlist) * 0.01 if 2 >= len(wordlist) * 0.01 else 0.2 )
 
-    time.sleep(2)
 
-time.sleep(10000)
 driver.close()
